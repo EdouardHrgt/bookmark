b6b539973b05e725ac67f6fd50c501e4c07902bb
Edouard Herrengt <86005588+EdouardHrgt@users.noreply.github.com>
2023-09-16 13:56:24 +0200

    all done

--- vue.js
+++ vue.js
@@ -20,9 +20,33 @@ const app = createApp({
       },
     ]);
 
+    const faqs = ref([
+      {
+        id: '1',
+        q: 'What is Bookmark?',
+        a: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum quis quam ornare mattis.',
+      },
+      {
+        id: '2',
+        q: 'How can I request a new browser?',
+        a: 'Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet. Vivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdie tVivamus luctus eros aliquet convallis ultricies. Mauris augue massa, ultricies non ligula. Suspendisse imperdiet.',
+      },
+      {
+        id: '3',
+        q: 'Is there a mobile app?',
+        a: 'Sed consectetur quam id neque fermentum accumsan. Praesent luctus vestibulum dolor, ut condimentum urna vulputate eget. Cras in ligula quis est pharetra mattis sit amet pharetra purus. Sed sollicitudin ex et ultricies bibendum.',
+      },
+      {
+        id: '4',
+        q: 'What about other Chromium browsers?',
+        a: 'Integer condimentum ipsum id imperdiet finibus. Vivamus in placerat mi, at euismod dui. Aliquam vitae neque eget nisl gravida pellentesque non ut velit.',
+      },
+    ]);
+
     let idx = ref(0);
     let isError = ref(false);
     let emailMsg = ref('');
+    let isOpen = ref(false);
     const checkEmail = (e) => {
       e.preventDefault();
       if (emailMsg.value.length <= 10) {
@@ -32,12 +56,24 @@ const app = createApp({
         alert('Your message has been sent!');
       }
     };
+
+    const handleFaq = (id) => {
+      if (isOpen.value === id) {
+        isOpen.value = false;
+      } else {
+        isOpen.value = id;
+      }
+    };
+
     return {
       bookmarks,
       idx,
       isError,
+      isOpen,
       emailMsg,
+      faqs,
       checkEmail,
+      handleFaq,
     };
   },
 });
